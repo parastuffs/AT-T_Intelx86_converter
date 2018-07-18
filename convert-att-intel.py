f02227d59edd328940894615dd251a4c8a955a55
Quentin Delhaye <parastuffs@gmail.com>
2018-07-18 16:58:29 +0200

    Switch parameters, convert pointers and immediates.

--- convert-att-intel.py
+++ convert-att-intel.py
@@ -12,9 +12,68 @@ Options:
 """
 
 from docopt import docopt
+import re
 
 def toATT(inputString):
 
+    commentBlock = False
+
+    # print inputString
+    with open("testfile.att", "r") as f:
+        inputString = f.read()
+
+    # print inputString
+    # First, split the input string in lines
+    lines = inputString.splitlines()
+    # print lines
+    # Remove extra spaces and tabulations
+    for i in range(len(lines)):
+        line = lines[i]
+        line = line.strip()
+
+        # Comment block detection
+        if line[0] + line[1] == "/*":
+            commentBlock = True
+            # print line
+        if "*/" in line:
+            commentBlock = False
+
+        if not commentBlock:
+            if "__asm__" in line:
+                line = "__asm{"
+            # Remove the '%'
+            line = re.sub(r'%', '', line)
+            # line = re.sub(r'%%?(?P<reg>\w{3,4})', '\g<reg>', line)
+            line = re.sub(r'"', '', line)
+            line = re.sub(r'\\n', '', line)
+
+            # Those should only be applied on instructions parameters to void facking up any hypothetical comment.
+            line = re.sub(r'\$(?P<imm>\d+)', '\g<imm>', line)
+            line = re.sub(r'\(', '[', line)
+            line = re.sub(r'\)', ']', line)
+
+            tokens = line.split(' ')
+            if len(tokens) > 1 and re.search(',.+', tokens[1]):
+                print tokens[1]
+                subtokens = tokens[1].split(',')
+                tokens[1] = subtokens[0]
+                tokens.insert(2, subtokens[1])
+                print tokens
+            # If there are two arguments and the second one is not a comment.
+            if len(tokens) > 2 and not re.search('^\/\/|^\/\*', tokens[2]):
+                tmp = tokens[1]
+                tokens[1] = tokens[2]
+                tokens[2] = tmp
+                print tokens
+
+
+
+
+        lines[i] = line
+
+
+
+    # print lines
 
 
 
@@ -26,22 +85,22 @@ def toATT(inputString):
 if __name__ == '__main__':
 
     inputString = ""
-    toATT = False
-    toIntel = False
+    isToATT = False
+    isToIntel = False
 
     args = docopt(__doc__)
     if args["-s"]:
         inputString = args["-s"]
     if args["--to-att"]:
-        toATT = True
+        isToATT = True
     if args["--to-intel"]:
-        toIntel = True
+        isToIntel = True
 
-    if toATT and toIntel:
+    if isToATT and isToIntel:
         print "Make up your mind."
         exit()
 
-    if toATT:
+    if isToATT:
         toATT(inputString)
-    elif toIntel:
+    elif isToIntel:
         toIntel(inputString)
