585d5202fbbc277c42758c6a164a3a1d141bcbcf
Quentin Delhaye <parastuffs@gmail.com>
2018-07-19 12:00:04 +0200

    Parse clobbers.

--- convert-att-intel.py
+++ convert-att-intel.py
@@ -19,6 +19,9 @@ def toATT(inputString):
     commentBlock = False
     tab = 0 # Number of tabulations at line beginning
     isLabel = False
+    clobbersCount = 0
+    variables = dict()
+    label = ""
 
     # print inputString
     with open("testfile.att", "r") as f:
@@ -27,6 +30,54 @@ def toATT(inputString):
     # print inputString
     # First, split the input string in lines
     lines = inputString.splitlines()
+
+
+    # I first need to parse the clobbers to figure out the name of the variables.
+    # The third clobber line can be dropped; it's simply the registers used.
+    # For the first two clobber lines, we have to build a dictionary with the
+    # index or label of the variable as key, and the name of the variable as value.
+    i = 0
+    while i < len(lines):
+        line = lines[i]
+        line = line.strip()
+
+        # If clobber line.
+        if re.search('^:', line):
+            del lines[i]
+            clobbersCount += 1
+            # If third clobber line, skip it.
+            if clobbersCount == 3:
+                continue
+            else:
+                # print line
+                line = re.sub(r'((\/\/)|(\/\*)).*', '', line) # Remove comments at the end of the line
+                # print line
+                tokens = line.split(',')
+                for j, token in enumerate(tokens):
+                    # Search for a label
+                    match = re.search('\[(\w+)\]', token)
+                    if match:
+                        label = match.group(1)
+                    else:
+                        label = str(clobbersCount - 1 + j)
+                    # Search for the variable name
+                    match = re.search('\((\w+)\)', token)
+                    if match:
+                        variables[label] = match.group(1)
+                    else:
+                        print("Error, could not find variable name.")
+
+        else:
+            i += 1
+
+    # print variables
+
+
+
+
+
+
+
     # print lines
     # Remove extra spaces and tabulations
     for i in range(len(lines)):
@@ -48,7 +99,6 @@ def toATT(inputString):
                 tab -= 1
             # Remove the '%'
             line = re.sub(r'%', '', line)
-            # line = re.sub(r'%%?(?P<reg>\w{3,4})', '\g<reg>', line)
             line = re.sub(r'"', '', line)
             line = re.sub(r'\\n', '', line)
 
@@ -67,7 +117,7 @@ def toATT(inputString):
                 tokens.insert(2, subtokens[1])
                 # print tokens
 
-            # If there are two arguments and the second one is not a comment.
+            # If there are two arguments and the second one is not a comment, swap them.
             if len(tokens) > 2 and not re.search('^\/\/|^\/\*', tokens[2]):
                 tmp = tokens[1]
                 tokens[1] = tokens[2] + ","
@@ -89,7 +139,7 @@ def toATT(inputString):
         lines[i] = "\t"*tab + line
         if isLabel:
             tab += 1
-        print lines[i]
+        # print lines[i]
 
 
 
